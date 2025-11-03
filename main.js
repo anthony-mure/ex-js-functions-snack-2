21a141d541b0ae3043ac8409db46a2a0a5e30e08
anthony-murè <tony.mure99@gmail.com>
2025-11-03 15:11:26 +0100

    [add]snack-3

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ Crea una arrow function che calcola il quadrato di un numero.
 -Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga. */
 
 const quadrato = n => n * n;
+
+/*SNACK 3
+Crea una funzione eseguiOperazione
+-Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri. */
+
+const moltiplica = (a, b) => a * b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
+
+console.log(eseguiOperazione(3, 4, moltiplica));
+
