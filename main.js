4bc663be360b034fc2af9748a25303913c9cea78
anthony-murè <tony.mure99@gmail.com>
2025-11-03 15:39:35 +0100

    [add]snack-4

--- main.js
+++ main.js
@@ -22,3 +22,19 @@ const eseguiOperazione = (a, b, operazione) => operazione(a, b);
 
 console.log(eseguiOperazione(3, 4, moltiplica));
 
+/*SNACK 4
+Crea un generatore di funzioni creaTimer
+-Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!". */
+
+function creaTimer(tempo) {
+
+  return () => {
+    setTimeout(() => {
+      console.log('Tempo scaduto!');
+    }, tempo);
+  }
+
+};
+
+const timer3s = creaTimer(3000);
+timer3s();
